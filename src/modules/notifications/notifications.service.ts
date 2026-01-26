4bd9fd32025e79dceb14c4f7c2c5965a3910735d
diaviso <diavisonoo94@gmail.com>
2026-01-26 00:39:48 +0000

    fix: update Prisma imports to use generated client path

--- src/modules/notifications/notifications.service.ts
+++ src/modules/notifications/notifications.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
-import { NotificationType } from '@prisma/client';
+import { NotificationType } from '../../generated/prisma';
 
 @Injectable()
 export class NotificationsService {
